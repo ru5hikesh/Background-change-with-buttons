e06dacc55f0dd7b75f1f2232fb5e7b2718960cfe
Rushikesh Kolge <124882090+ru5hikesh@users.noreply.github.com>
2024-09-18 01:03:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+function red() {
+    document.body.style.backgroundColor = "red";
+}
+
+function yellow() {
+    document.body.style.backgroundColor = "yellow";
+}
+
+function green() {
+    document.body.style.backgroundColor = "green";
+}
+
+function blue() {
+    document.body.style.backgroundColor = "blue";
+}
+
+function purple() {
+    document.body.style.backgroundColor = "purple";
+}
+
+function orange() {
+    document.body.style.backgroundColor = "orange";
+}
+
+function black() {
+    document.body.style.backgroundColor = "black";
+}
+
+function white() {
+    document.body.style.backgroundColor = "white";
+}
+
+
+
